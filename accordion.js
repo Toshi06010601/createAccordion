bedf914d45482b631700cb9231a480341d73f6d1
Toshinobu Nakamura <nkmr104@outlook.com>
2024-11-10 14:14:39 +0000

    use class and instance

--- accordion.js
+++ accordion.js
@@ -1,29 +1,49 @@
 (() => {
-    const $elm = document.querySelector('#js-accordion');
-    const $trigger = $elm.getElementsByTagName('a');
-    const triggerLen = $trigger.length;
 
-    let index = 0;
-    while(index < triggerLen) {
-        $trigger[index].addEventListener("click", (e) => clickHandler(e));
-        index++;
-    }
+    class Accordion {
+        //初期化
+        constructor(obj) {
+            const $elm = document.querySelector(obj.hookName);
+            const $trigger = $elm.getElementsByTagName(obj.tagName);
+            const triggerLen = $trigger.length;
 
-    //clickしたら実行される処理
-    const clickHandler = (e) => {
-        e.preventDefault();
-        
-        const $target = e.currentTarget;        
-        const $content = $target.nextElementSibling;
-
-        if($content.style.display === "block") {
-            $content.style.display = "none";
-        } else {
-            $content.style.display = "block"
+            let index = 0;
+            while (index < triggerLen) {
+                $trigger[index].addEventListener("click", (e) => this.clickHandler(e));
+                index++;
+            }
         }
-        
-        
-    }
-}
 
-)();
+        //clickしたら実行される処理
+        clickHandler(e) {
+            e.preventDefault();
+
+            const $target = e.currentTarget;
+            const $content = $target.nextElementSibling;
+
+            if ($content.style.display === "block") {
+                $content.style.display = "none";
+            } else {
+                $content.style.display = "block"
+            }
+
+        }
+
+    }
+
+    const fuckingFaq = new Accordion({
+        hookName: '#js-faq',
+        tagName: "p"
+    });
+
+    const fuckingAccordion = new Accordion({
+        hookName: '#js-accordion',
+        tagName: "a"
+    });
+
+    const fuckingMiniAccordion = new Accordion({
+        hookName: '#js-accordion-mini',
+        tagName: "dt"
+    });
+
+})();
